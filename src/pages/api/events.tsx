87a48113da7caf3d0a77ad5cefbb1f114381de4c
Hugo Villevieille <hugo.villevieille@zol.fr>
2023-07-17 12:15:56 +0200

    [#] adjusted route

--- src/pages/api/events.tsx
+++ src/pages/api/events.tsx
@@ -1,10 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const EVENT_TYPES = {
+    URL_VERIFICATION: 'url_verification',
+}
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        const { challenge } = req.query;
+        const { type, token, challenge } = req.body;
 
-        res.status(200).send(challenge);
+        switch (type) {
+            case (EVENT_TYPES.URL_VERIFICATION): {
+                res.status(200).send(challenge);
+                break;
+            }
+            default: break;
+        }
     }
     else {
         res.status(405).end();
